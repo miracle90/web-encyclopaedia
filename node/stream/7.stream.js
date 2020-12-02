ada25adcb1079cea9083bb25138400778b9130ea
李亚运(11112733) <11112733@vivo.xyz>
2020-12-02 16:06:00 +0800

    流

--- node/stream/7.stream.js
+++ node/stream/7.stream.js
@@ -1,5 +1,7 @@
 let fs = require('fs')
-const { Writable, Readable } = require('stream')
+const { on } = require('process')
+const { Writable, Readable, Duplex, Transform } = require('stream')
+const Stream = require('stream')
 
 // 父类会调用子类 ReadStream 的 _read 方法
 // read 方法是父类的方法，Readable，read()
@@ -26,7 +28,11 @@ class MyWriteStream extends Writable {
   // 子类实现可写流方法
   _write(chunk, encoding, cb) {
     console.log(chunk)
-    cb()
+    fs.appendFile('./name.txt', chunk, () => {
+      setTimeout(() => {
+        cb()
+      }, 1000);
+    })
   }
 }
 
@@ -37,7 +43,60 @@ class MyWriteStream extends Writable {
 // })
 
 // 可写流
-let ws = new MyWriteStream()
-ws.write('1')
-ws.write('2')
+// let ws = new MyWriteStream()
+// ws.write('1')
+// ws.write('2')
+
+// 双工流，读写都可以的流，Duplex
+// util.inherits(Duplex, Readable)
+// util.inherits(Duplex, Writable)
+class MyDuplex extends Duplex {
+  _read() {
+    console.log('read')
+  }
+  _write(chunk) {
+    console.log('chunk ', chunk)
+  }
+}
+// let dl = new MyDuplex()
+// dl.on('data', () => {
+  
+// })
+// dl.write('666')
+
+// rs.pipe(ts).pipe(ws)
+// 压缩，gzip，转化流，先把数据读取出来 => 进行转化 => 写到一个新的文件里
+class MyTransfrom extends Transform {
+  _transform(chunk, encoding, cb) {
+    this.push(chunk.toString().toUppercase())
+    cb()
+  }
+}
+
+// 转化，把写转化成读
+let myTransfrom = new MyTransfrom()
+process.stdin.pipe(myTransfrom).pipe(process.stdout)
+// myTransfrom.on('data', function (data) {
+//   console.log('data ', data)
+// })
+// myTransfrom.write('666')
+
+
+
+// 读流，写流，双工流，转化流
+
+// 进程中通信
+// process.stdin.on('data', chunk => {
+//   console.log('process.stdin ', chunk)
+// })    // 只要能on(''data) => 流
+// process.stdout.write('000')   // 只要有write，就是可写流
+
+
+// 常用方法
+// pipe
+// rs.on('data')
+// rs.on('end')
+// ws.write()
+// ws.end()
+
 
