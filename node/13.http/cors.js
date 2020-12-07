70af44f55d64543958945b3feaa0099b797cddad
李亚运(11112733) <11112733@vivo.xyz>
2020-12-07 17:56:10 +0800

    cors

--- node/13.http/cors.js
+++ node/13.http/cors.js
@@ -1,1 +1,13 @@
 // cors跨域
+let http = require('http')
+let fs = require('fs')
+let url = require('url')
+let path = require('path')
+
+let server = http.createServer((req, res) => {
+  let { pathname } = url.parse(req.url)
+  let absPath = path.join(__dirname, pathname)
+  fs.stat(absPath, function (err, statObj) {
+    if ()
+  })
+})
